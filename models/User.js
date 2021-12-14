34d2ebff263e82e76562273c15891bd00134c5dd
Nemesis <szakacsmpaula@gmail.com>
2021-12-14 15:44:02 +0100

    add changes in notes users dotenv

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
     name: {"type": String, "required": true},
     username: {"type": String, "required": true},
-    password: {"type": String, "required": true},
+    hash: {"type": String, "required": true},
     email: {"type": String, "required": true},
 }, {
     timestamps: true,
--- server.js
+++ server.js
@@ -1,77 +1,132 @@
 import express from "express";
-import mongoose from "mongoose";
 import session from "express-session";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 import UserModel from "./models/User.js";
+import bcrypt from "bcrypt";
 
 dotenv.config();
-
 mongoose.connect(process.env.MONGOURI);
 
 const app = express();
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 
-app.use(cookieParser());
+app.use(express.json());
+
+app.set("trust proxy", 1);
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_ORIGIN,
     credentials: true,
   })
 );
 
-app.use(express.json());
+app.use(cookieParser());
 app.use(
   session({
+    secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
-    secret: process.env.SESSION_SECRET || "use-env-secret728272",
+    cookie: {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+    },
   })
 );
 
-app.post("/login", (req, res) => {
+const userIsInGroup = (user, accessGroup) => {
+  const accessGroupArray = user.accessGroups.split(",").map((m) => m.trim());
+  return accessGroupArray.includes(accessGroup);
+};
+
+app.post("/login", async (req, res) => {
   const username = req.body.username;
   // const password = req.body.password;
-  let user = users.find((user) => user.username === username);
+  let user = await UserModel.findOne({ username });
   if (!user) {
-    user = users.find((user) => user.username === "anonymousUser");
+    user = await UserModel.findOne({ username: "anonymousUser" });
   }
   req.session.user = user;
   req.session.save();
   res.json(user);
 });
 
+app.post("/signup", async (req, res) => {
+  const frontUser = req.body.user;
+  if (
+    frontUser.username.trim() === "" ||
+    frontUser.password1.trim() === "" ||
+    frontUser.password1 !== frontUser.password2
+  ) {
+    res.sendStatus(403);
+  } else {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(frontUser.password1, salt);
+    const dbuser = await UserModel.create({
+      login: frontUser.login,
+      firstName: frontUser.firstName,
+      lastName: frontUser.lastName,
+      email: frontUser.email,
+      hash,
+      accessGroups: "loggedInUsers,notYetApprovedUsers",
+    });
+    req.session.user = dbuser;
+    req.session.save();
+    res.json(dbuser);
+  }
+});
+
 app.get("/currentuser", async (req, res) => {
+  console.log("hello");
   let user = req.session.user;
   if (!user) {
-    user = await UserModel.findOne({ username: "anonymousUser" });
+    user = await UserModel.findOne({ login: "anonymousUser" });
   }
-  res.json(user);
+  res.json({
+    user,
+  });
 });
 
-app.get("/logout", (req, res) => {
+app.post("/approveuser", async (req, res) => {
+  const id = req.body.id;
+  let user = req.session.user;
+  if (!user) {
+    res.sendStatus(403);
+  } else {
+    if (!userIsInGroup(user, "admins")) {
+      res.sendStatus(403);
+    } else {
+      const updateResult = await UserModel.findOneAndUpdate(
+        { _id: new mongoose.Types.ObjectId(id) },
+        { $set: { accessGroups: "loggedInUsers,members" } },
+        { new: true }
+      );
+      res.json({
+        result: updateResult,
+      });
+    }
+  }
+});
+
+app.get("/notyetapprovedusers", async (req, res) => {
+  const users = await UserModel.find({
+    accessGroups: { $regex: "notYetApprovedUsers", $options: "i" },
+  });
+  res.json({
+    users,
+  });
+});
+
+app.get("/logout", async (req, res) => {
   req.session.destroy();
-  const user = users.find((user) => user.username === "anonymousUser");
-  res.json(user);
-});
-
-app.get("/testing", (req, res) => {
-  const pw = "pau123"; // PLain text password
-
-  const hashedPw = bcrypt.hashSync(pw);
-
-  res.send(hashedPw);
-});
-app.get("/user", async (req, res) => {
-  const users = await User.find();
-  res.send(users);
-});
-
-app.post("/user", async (req, res) => {
-  const user = await User.create(req.body);
-
-  res.send(user);
+  const user = await UserModel.findOne({ login: "anonymousUser" });
+  res.json({
+    user,
+  });
 });
 
 app.listen(PORT, (req, res) => {
